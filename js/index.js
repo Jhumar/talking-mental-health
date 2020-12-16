a8a959ab679edc550f9d0cf1b5ed9e926f98f19d
Jhumar <rjhumar27@gmail.com>
2020-12-16 16:26:41 +0800

    added carousel slideshow functionalities

--- js/index.js
+++ js/index.js
@@ -10,3 +10,38 @@ function handleScroll(e) {
     header.classList.remove('header-active');
   }
 }
+
+const carouselItems = document.querySelectorAll('.carousel-slideshow-item');
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+let currentItem = 0;
+
+const handleButtonPrev = () => {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = carouselItems.length - 1;
+  }
+
+  showCurrentSlide();
+};
+
+const handleButtonNext = () => {
+  currentItem++;
+  if (currentItem > carouselItems.length - 1) {
+    currentItem = 0;
+  }
+  
+  showCurrentSlide();
+};
+
+const showCurrentSlide = () => {
+  carouselItems.forEach((item, index) => {
+    item.classList.remove('carousel-slideshow-item-current');
+    if (index == currentItem) {
+      item.classList.add('carousel-slideshow-item-current');
+    }
+  });
+};
+
+btnPrev?.addEventListener('click', handleButtonPrev);
+btnNext?.addEventListener('click', handleButtonNext);
